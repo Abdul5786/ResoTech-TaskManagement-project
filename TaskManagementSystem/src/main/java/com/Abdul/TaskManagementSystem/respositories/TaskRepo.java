266d3b09903dbc5c30d4c9455905766a86c12053
Abdul khaliq <99346275+Abdul5786@users.noreply.github.com>
2024-01-06 13:17:18 +0530

    Add files via upload

---
+++ TaskManagementSystem/src/main/java/com/Abdul/TaskManagementSystem/respositories/TaskRepo.java
@@ -0,0 +1,8 @@
+package com.Abdul.TaskManagementSystem.respositories;
+
+import com.Abdul.TaskManagementSystem.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepo extends JpaRepository<Task,Integer>
+{
+}
